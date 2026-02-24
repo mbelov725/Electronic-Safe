c1073ffe917719983479ab25f9147a21eaf6bad0
mbelov725 <mark.belov725@gmail.com>
2026-02-24 18:25:44 -0500

    Initial commit. Added code for icons.

---
+++ include/icons.h
@@ -0,0 +1,16 @@
+// Only compile file once
+#ifndef ICONS_H
+#define ICONS_H
+
+#include "LiquidCrystal.h"
+
+// Create custom character numbers
+#define ICON_LOCKED_CHAR (byte)0
+#define ICON_UNLOCKED_CHAR (byte)1
+
+// Standard icon on LCD1602 character set
+#define ICON_RIGHT_ARROW (byte)126
+
+void init_icons(LiquidCrystal &lcd);
+
+#endif
---
+++ src/icons.cpp
@@ -0,0 +1,38 @@
+#include "Arduino.h"
+#include "icons.h"
+
+// Pixel drawing of locked and unlocked icon
+// Stored in flash memory, which is large and non-volatile, as opposed to RAM
+const byte icon_locked[8] PROGMEM = {
+    0b01110,
+    0b10001,
+    0b10001,
+    0b11111,
+    0b11011,
+    0b11011,
+    0b11111,
+};
+
+const byte icon_unlocked[8] PROGMEM = {
+    0b01110,
+    0b10000,
+    0b10000,
+    0b11111,
+    0b11011,
+    0b11011,
+    0b11111,
+};
+
+// Copies icon data from from flash memory
+// Uploads it to LCD memory
+void init_icons(LiquidCrystal &lcd) {
+    // Temporary RAM buffer, as LCD cannot read from PROGMEM directly
+    byte icon[8];
+
+    // Copy from flash memory
+    // Upload to LCD
+    memcpy_P(icon, icon_locked, sizeof(icon));
+    lcd.createChar(ICON_LOCKED_CHAR, icon);
+    memcpy_P(icon, icon_unlocked, sizeof(icon));
+    lcd.createChar(ICON_UNLOCKED_CHAR, icon);
+}
